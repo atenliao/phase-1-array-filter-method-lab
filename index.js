17241ee827ef3d41e2a8d1ff20ac4282e59f114e
Zhongming <atenliao@gmail.com>
2023-03-25 21:46:53 -0700

    complete index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+const findMatching = (arr, str) =>{
+    return arr.filter((input_str)=>{
+       return input_str.toUpperCase() === str.toUpperCase()
+    })
+}
+
+const fuzzyMatch = (arr, str) => {
+    return arr.filter((input_str)=>{
+           if(input_str.indexOf(str) === 0){
+            return input_str.includes(str)
+           }
+        
+                
+    })
+}
+
+
+const matchName =(obj, check_name) => {
+    return obj.filter(({name})=>{
+        return name === check_name
+    })
+}
